acf997199a47a6826e2f5e2dc4051f1d2e4c42c7
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-11-29 10:47:52 +0100

    Agregar archivos de proyecto.

---
+++ UTF4-EJER1/bordes.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UTF4_EJER1
+{
+    class bordes : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int borde;
+            if ((bool)value)
+                borde = 3;
+            else
+                borde = 0;
+            return borde;
+                
+           
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int borde = 0;
+            return borde;
+        }
+    }
+
+
+}
